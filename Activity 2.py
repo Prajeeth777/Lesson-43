0d9b9a3800258f78c79b28157dd5d09fc762ce0c
Prajeeth777 <prajeethkumar777@gmail.com>
2025-01-04 20:07:37 +0900

    Create Activity 2.py

---
+++ Activity 2.py
@@ -0,0 +1,15 @@
+dict={'Name':'Seong Gi-hun','Age': 47}
+
+print(dict)
+
+dict['Gender']='Male'
+
+print("Updated dictionary is ",dict)
+
+dict['Age']=48
+
+print("Updated dictionary is ",dict)
+
+dict.pop('Age')
+
+print("Updated dictionary is ",dict)
